8f80bc411022a85b0d331430faf2fabc3876df2c
Loupo5 <209892110+Loupo5@users.noreply.github.com>
2026-07-03 16:55:44 +0200

    Make the grid ui
    The grid actually fills the whole screen.
    I used a inner loop to create 16 different divs
    that contain 16 different childDivs

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const container = document.querySelector(".container")
 for (let j=0; j<16; j++) {
-    const part = document.createElement("div")
-    part.classList.add("one16th")
-    container.appendChild(part)
+    const divContainer = document.createElement("div")
+    divContainer.classList.add("divContainer")
+    container.appendChild(divContainer)
     for (let i=0; i<16; i++) {
         const div = document.createElement("div")
         div.classList.add("partOfGrid")
         div.textContent = "KAREN"
-        part.appendChild(div)
+        divContainer.appendChild(div)
     }
 }
