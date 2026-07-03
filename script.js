6c655138964cfce5f572fcedd3e74782bb5ebddd
Loupo5 <209892110+Loupo5@users.noreply.github.com>
2026-07-03 20:51:35 +0200

    SKIP

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ for (let j=0; j<16; j++) {
         div.classList.add("partOfGrid")
         div.style.width = "500/16px"
         div.style.height = "500/16px"
+        div.textContent = "NIGGAAA"
         divContainer.appendChild(div)
         div.addEventListener("mouseenter", (e) => {
-            div.style.backgroundColor = 'blue'
+            div.style.backgroundColor = 'black'
         })
     }
 }
