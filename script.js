c96a10df6aabc42e2043972ffa46060f075a1805
Loupo5 <209892110+Loupo5@users.noreply.github.com>
2026-07-03 18:22:48 +0200

    Make the grid and the divs are perfect squares
    Made it so the grid is 500px wide and high so
    the divs can easier fit into the space to make
    perfect squares. Now i have to make it so the user
    inputs the number of divs inside the div.

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ for (let j=0; j<16; j++) {
     for (let i=0; i<16; i++) {
         const div = document.createElement("div")
         div.classList.add("partOfGrid")
-        div.textContent = "KAREN"
+        div.style.width = "500/16px"
+        div.style.height = "500/16px"
         divContainer.appendChild(div)
+        div.addEventListener("mouseenter", (e) => {
+            div.style.backgroundColor = 'blue'
+        })
     }
 }
