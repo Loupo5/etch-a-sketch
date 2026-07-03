c1eb16dd22c5ece728760e6acda3e6decdb15fa8
Loupo5 <209892110+Loupo5@users.noreply.github.com>
2026-07-03 15:13:17 +0200

    Make my main container the size of the page
    Made it so the drawing takes your whole screen,
    have to figure out how to create the 16x16 grid.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("KAREN")
+const container = document.querySelector(".container")
+for (let j=0; j<16; j++) {
+    const part = document.createElement("div")
+    part.classList.add("one16th")
+    container.appendChild(part)
+    for (let i=0; i<16; i++) {
+        const div = document.createElement("div")
+        div.classList.add("partOfGrid")
+        div.textContent = "KAREN"
+        part.appendChild(div)
+    }
+}
