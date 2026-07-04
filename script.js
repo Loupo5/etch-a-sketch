4a179a4661b5179117da81c29d926a60d38a821f
Loupo5 <209892110+Loupo5@users.noreply.github.com>
2026-07-04 08:02:15 +0200

    Make the grid take input of the user
    The grid now changes based on user input.
    It's only one color now. Have to make it change
    colors.

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 const container = document.querySelector(".container")
-for (let j=0; j<16; j++) {
-    const divContainer = document.createElement("div")
-    divContainer.classList.add("divContainer")
-    container.appendChild(divContainer)
-    for (let i=0; i<16; i++) {
-        const div = document.createElement("div")
-        div.classList.add("partOfGrid")
-        div.style.width = "500/16px"
-        div.style.height = "500/16px"
-        divContainer.appendChild(div)
-        div.addEventListener("mouseenter", (e) => {
-            div.style.backgroundColor = 'lightgreen'
-        })
+let num = 16
+makeGrid(num)
+function makeGrid(num) {
+    for (let j=0; j<num; j++) {
+        const divContainer = document.createElement("div")
+        divContainer.classList.add("divContainer")
+        container.appendChild(divContainer)
+        for (let i=0; i<num; i++) {
+            const div = document.createElement("div")
+            div.classList.add("partOfGrid")
+            div.style.width = "(500/num)px"
+            div.style.height = "(500/num)px"
+            divContainer.appendChild(div)
+            div.addEventListener("mouseenter", (e) => {
+                div.style.backgroundColor = 'lightgreen'
+            })
+        }
     }
 }
 
 const btn = document.querySelector("button")
 btn.addEventListener("click", (e) => {
-    let num = prompt("How many grids per side do you want?", 16)
+    num = prompt("How many grids per side do you want?")
+    if (num < 0 || num > 100) {
+        prompt("ERROR - you can't load that number of grids")
+    }
+    if (!num) return "ERROR - please input a number"
+
+    container.textContent = ""
+    makeGrid(num)
+
 })
